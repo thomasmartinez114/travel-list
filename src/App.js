55d8d528e3b2ce5a0ae06ed55df8832b835a9ef1
Thomas Martinez <devtommie114@gmail.com>
2025-10-09 02:07:36 -0400

    deleting item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 3, description: 'Charger', quantity: 1, packed: true },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
@@ -13,11 +7,16 @@ export default function App() {
     setItems(items => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    // console.log(id);
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
-    <div className="app">
+    <div className='app'>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -47,7 +46,7 @@ function Form({ onAddItems }) {
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select
         value={quantity}
@@ -60,8 +59,8 @@ function Form({ onAddItems }) {
         ))}
       </select>
       <input
-        type="text"
-        placeholder="Item...."
+        type='text'
+        placeholder='Item....'
         value={description}
         onChange={e => setDescription(e.target.value)}
       />
@@ -70,32 +69,32 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
-    <div className="list">
+    <div className='list'>
       <ul>
         {items.map(item => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
 function Stats() {
   return (
-    <footer className="stats">
+    <footer className='stats'>
       <em>💼 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
