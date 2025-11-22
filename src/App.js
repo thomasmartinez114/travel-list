a16fa3c5deecdf276e8c232f54c877cd21938f26
Thomas Martinez <devtommie114@gmail.com>
2025-11-22 00:17:57 -0500

    added confirm message

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ export default function App() {
   }
 
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm(
+      'Are you sure you want to delete all items?'
+    );
+
+    if (confirmed) setItems([]);
   }
 
   return (
